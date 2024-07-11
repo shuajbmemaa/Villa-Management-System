784970601b2e2f10629455078a6131644b263ada
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-11 12:42:56 +0200

    Connection with DB

---
+++ Villa.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+        }
+    }
+}
