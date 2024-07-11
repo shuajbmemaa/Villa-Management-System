33bc182dd3f8e1c013f96cbf8351fb37cd920ba4
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-11 14:38:24 +0200

    Create Hotel Controller

---
+++ Villa/Controllers/HotelController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa.Infrastructure.Data;
+
+namespace Villa.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public HotelController(ApplicationDbContext db)
+        {
+            _db = db;           
+        }
+        public IActionResult Index()
+        {
+            var hotels = _db.Hotels.ToList();
+            return View(hotels);
+        }
+    }
+}
