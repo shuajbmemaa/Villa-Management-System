a4c2474a005f8c5c1c1d9944830d3720f55ba03b
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-12 12:03:47 +0200

    Added some data annotations for some specific attributes

--- Villa.Domain/Entities/Hotel.cs
+++ Villa.Domain/Entities/Hotel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,18 @@ namespace Villa.Domain.Entities
     public class Hotel
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public required string Name { get; set; }
 
         public string? Description { get; set; }
 
+        [Display(Name = "Price per night")]
+        [Range(10,1000)]
         public double Price { get; set; }
         public int Area { get; set; }
+        [Range(1,10)]
         public int Occupancy { get; set; }
+        [Display(Name ="Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
         public DateTime? Updated_Date { get;set; }
