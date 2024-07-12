34b9af8ec3aaea79ef9b8428c010f2cb9fbb1328
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-12 21:13:15 +0200

    CRUD for Hotel

--- Villa/Controllers/HotelController.cs
+++ Villa/Controllers/HotelController.cs
@@ -38,5 +38,50 @@ namespace Villa.Controllers
             }
             return View(hotel);
         }
+
+        public IActionResult Update(int hotelId)//qysh e ke lan n Index.cshtml te asp-action duhet me kan edhe emri i metodes , poashtu cka pranon si parameter duhet me kan se cka i ke caktu ti te asp-route-...
+        {
+            Hotel? hotel=_db.Hotels.FirstOrDefault(h => h.Id == hotelId);
+            if(hotel == null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+            return View(hotel) ;
+        }
+
+        [HttpPost]
+        public IActionResult Update(Hotel hotel)
+        {
+            if (ModelState.IsValid && hotel.Id >0)
+            {
+                _db.Hotels.Update(hotel);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Hotel");
+            }
+            return View(hotel);
+        }
+
+        public IActionResult Delete(int hotelId)//qysh e ke lan n Index.cshtml te asp-action duhet me kan edhe emri i metodes , poashtu cka pranon si parameter duhet me kan se cka i ke caktu ti te asp-route-...
+        {
+            Hotel? hotel = _db.Hotels.FirstOrDefault(h => h.Id == hotelId);
+            if (hotel == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(hotel);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Hotel hotel)
+        {
+            Hotel? objfromDB=_db.Hotels.FirstOrDefault(h=>h.Id == hotel.Id);
+            if (objfromDB is not null)
+            {
+                _db.Hotels.Remove(objfromDB);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Hotel");
+            }
+            return View(hotel);
+        }
     }
 }
