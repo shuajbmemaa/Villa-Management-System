a4434a49636d2e2af2d88ff764c958a10a5b490f
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-13 17:39:48 +0200

    ViewModel for Hotel Number

---
+++ Villa/ViewModels/HotelNumberVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Villa.Domain.Entities;
+
+namespace Villa.ViewModels
+{
+    public class HotelNumberVM
+    {
+        public HotelNumber? HotelNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? HotelList { get; set; }
+
+
+    }
+}
