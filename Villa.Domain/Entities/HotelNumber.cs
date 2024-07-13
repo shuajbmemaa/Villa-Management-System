95934def4aec14bd4d455b77e93caa937f45fe2c
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-13 12:34:15 +0200

    Update the landing page and added a new entity

---
+++ Villa.Domain/Entities/HotelNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Domain.Entities
+{
+    public class HotelNumber
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Hotel_Nr { get; set; }
+
+        [ForeignKey("Hotel")]
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+        public string? SpecialDetails { get; set; }
+    }
+}
