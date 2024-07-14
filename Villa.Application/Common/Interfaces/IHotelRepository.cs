5e70781cbb5804d898787dcefd5253d050572bd8
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-14 17:18:09 +0200

    Hotel Repository Interface

---
+++ Villa.Application/Common/Interfaces/IHotelRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Domain.Entities;
+
+namespace Villa.Application.Common.Interfaces
+{
+    public interface IHotelRepository
+    {
+        IEnumerable<Hotel> GetAll(Expression<Func<Hotel,bool>>? filter = null,string? includeProperties = null);        IEnumerable<Hotel> GetAll(Expression<Func<Hotel,bool>>? filter = null,string? includeProperties = null);
+        IEnumerable<Hotel> Get(Expression<Func<Hotel, bool>> filter, string? includeProperties = null);
+        void Add(Hotel hotel);
+        void Update(Hotel hotel);
+        void Remove(Hotel hotel);
+        void Save();
+
+
+    }
+}
