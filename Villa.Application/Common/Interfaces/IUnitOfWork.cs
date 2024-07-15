ede706c716749964b5821f5da3059de55d896738
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-15 23:21:04 +0200

    Implemented (I)UnitOfWork

---
+++ Villa.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IHotelRepository Hotel { get; }
+        IHotelNumberRepository HotelNumber { get; }
+
+        void Save();
+
+    }
+}
---
+++ Villa.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Application.Common.Interfaces;
+using Villa.Infrastructure.Data;
+
+namespace Villa.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IHotelRepository Hotel {  get; private set; }
+
+        public IHotelNumberRepository HotelNumber {  get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db=db;
+            Hotel = new HotelRepository(_db);
+            HotelNumber= new HotelNumberRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
