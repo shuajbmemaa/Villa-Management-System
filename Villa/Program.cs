ae139eb7b88a2ef1ab19f8a4be56d58351b20818
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-15 23:21:57 +0200

    Generic form for services using UnitOfWork

--- Villa/Program.cs
+++ Villa/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IHotelRepository,HotelRepository>();
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 
 var app = builder.Build();
 
