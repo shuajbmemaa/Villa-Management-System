ce6465358853bab360d835b71c00fc86772d08bb
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-17 11:20:15 +0200

    Added amenity table with its attributes

---
+++ Villa.Domain/Entities/Amenity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Domain.Entities
+{
+    public class Amenity
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+
+        [ForeignKey("Hotel")]
+        public int HotelId { get; set; }
+        [ValidateNever]
+        public Hotel Hotel { get; set; }
+
+    }
+}
