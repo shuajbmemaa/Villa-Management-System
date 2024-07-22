8ba835329bc0ce84f7f008206a3ea6b96164ce4f
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-22 14:58:47 +0200

    View Models for Login&Register

---
+++ Villa/ViewModels/LoginVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villa.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+        public string? RedirectUrl { get; set; }
+
+    }
+}
---
+++ Villa/ViewModels/RegisterVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villa.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name ="Confirm Password")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name ="Phone Number")]
+        public string? PhoneNumber { get; set; }
+
+        public string? RedirectUrl { get; set; }
+    }
+}
