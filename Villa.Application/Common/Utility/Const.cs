b8be381d510696de9fcb12f56825fc3e85146e2b
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-22 21:05:33 +0200

    Created a file only for static data

---
+++ Villa.Application/Common/Utility/Const.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Application.Common.Utility
+{
+    public static class Const
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Admin = "Admin";
+
+    }
+}
