a42ba87f6d6ec913e0543cf49c0da901e2c49a5b
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-22 16:00:44 +0200

    Views,ViewModel and Controller for Register

---
+++ Villa/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Villa.Application.Common.Interfaces;
+using Villa.Domain.Entities;
+using Villa.ViewModels;
+
+namespace Villa.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;//access to the db
+        private readonly UserManager<User> _userManager;//manage user
+        private readonly SignInManager<User> _signInManager;//login user
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(IUnitOfWork unitOfWork,
+            UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<User> signInManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+        //get
+        public IActionResult Login(string returnUrl=null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            LoginVM loginVM = new ()
+            {
+                RedirectUrl = returnUrl
+            };
+
+            return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            if (!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();//e krijon rolin admin ne aspnetroles
+                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();//roli customer
+            }
+
+            //dropdown mi shfaq rolet
+            RegisterVM registerVM = new ()
+            {
+                RoleList=_roleManager.Roles.Select(x=> new SelectListItem
+                {
+                    Text=x.Name,
+                    Value=x.Name
+                })
+            };
+            return View(registerVM);
+        }
+    }
+}
--- Villa/ViewModels/RegisterVM.cs
+++ Villa/ViewModels/RegisterVM.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace Villa.ViewModels
 {
@@ -22,5 +24,11 @@ namespace Villa.ViewModels
         public string? PhoneNumber { get; set; }
 
         public string? RedirectUrl { get; set; }
+
+        public string? Role { get; set; }
+
+        //dropdown per role
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? RoleList { get; set; }
     }
 }
