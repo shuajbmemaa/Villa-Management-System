bb4d66e71015522c6e3c72dd21299b7556c7f7b2
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-26 16:09:00 +0200

    Added constants for status values

--- Villa.Application/Common/Utility/Const.cs
+++ Villa.Application/Common/Utility/Const.cs
@@ -11,5 +11,12 @@ namespace Villa.Application.Common.Utility
         public const string Role_Customer = "Customer";
         public const string Role_Admin = "Admin";
 
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusCheckedIn = "CheckedIn";
+        public const string StatusCompleted = "Completed";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
     }
 }
