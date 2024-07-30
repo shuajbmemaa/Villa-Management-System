2e020385993359b7bf238a0583bae99950158170
shuajbkingu <shuajbmemaa@hotmail.com>
2024-07-30 20:50:26 +0200

    Added a method to look for available rooms

--- Villa.Application/Common/Utility/Const.cs
+++ Villa.Application/Common/Utility/Const.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Villa.Domain.Entities;
 
 namespace Villa.Application.Common.Utility
 {
@@ -17,6 +18,44 @@ namespace Villa.Application.Common.Utility
         public const string StatusCompleted = "Completed";
         public const string StatusCancelled = "Cancelled";
         public const string StatusRefunded = "Refunded";
+
+        public static int HotelRoomsAvailable(int hotelId,List<HotelNumber> hotelNumberList,
+                                              DateOnly checkInDate,int nights,List<Booking> bookings)
+        {
+            List<int> bookingInDate = new();
+            int finalAvailableRoom = int.MaxValue;
+
+            var roomsinHotel=hotelNumberList.Where(u=>u.HotelId == hotelId).Count();
+
+            for(int i=0;i<nights;i++)
+            {
+                var villasBooked = bookings.Where(u => u.CheckInDate <= checkInDate.AddDays(i) && u.CheckOutDate>=checkInDate.AddDays(i)
+                                                    && u.HotelId == hotelId);
+
+                foreach(var booking in villasBooked)
+                {
+                    if (!bookingInDate.Contains(booking.Id))
+                    {
+                        bookingInDate.Add(booking.Id);
+                    }
+                }
+
+                var totalAvailableRooms=roomsinHotel - bookingInDate.Count;
+                if(totalAvailableRooms == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (finalAvailableRoom > totalAvailableRooms)
+                    {
+                        finalAvailableRoom = totalAvailableRooms;
+                    }
+                }
+
+            }
+            return finalAvailableRoom;
+        }
 
     }
 }
